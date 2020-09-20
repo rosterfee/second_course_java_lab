e6e32ac91744eb62e0ac5f83f69029d5db70cdb5
Ildar <dakalom@mail.ru>
2020-09-20 16:30:32 +0300

    homework

---
+++ src/ru/kpfu/itis/group905/kiyamdinov/Main.java
@@ -0,0 +1,31 @@
+package ru.kpfu.itis.group905.kiyamdinov;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Runnable task1 = () -> {
+            for (int i = 0; i < 100; i++) {
+                System.out.println(Thread.currentThread().getName() + " A");
+            }
+        };
+
+        Runnable task2 = () -> {
+            for (int i = 0; i < 100; i++) {
+                System.out.println(Thread.currentThread().getName() + " B");
+            }
+        };
+
+        Runnable task3 = () -> {
+            for (int i = 0; i < 100; i++) {
+                System.out.println(Thread.currentThread().getName() + " C");
+            }
+        };
+
+
+        ThreadPool threadPool = ThreadPool.newPool(3);
+
+        threadPool.submit(task1);
+        threadPool.submit(task2);
+        threadPool.submit(task3);
+    }
+}
---
+++ src/ru/kpfu/itis/group905/kiyamdinov/ThreadPool.java
@@ -0,0 +1,57 @@
+package ru.kpfu.itis.group905.kiyamdinov;
+
+import java.util.Deque;
+import java.util.concurrent.ConcurrentLinkedDeque;
+
+public class ThreadPool {
+
+    private static Deque<Runnable> tasks;
+    private PoolWorker[] pool;
+
+    public static ThreadPool newPool(int threadsCount) {
+        ThreadPool threadPool = new ThreadPool();
+        tasks = new ConcurrentLinkedDeque<>();
+        threadPool.pool = new PoolWorker[threadsCount];
+
+        for (int i = 0; i < threadPool.pool.length; i++) {
+            threadPool.pool[i] = new PoolWorker();
+            threadPool.pool[i].start();
+        }
+        return threadPool;
+    }
+
+
+    public void submit(Runnable task) {
+        synchronized (tasks) {
+            tasks.addLast(task);
+            tasks.notify();
+        }
+
+    }
+
+    private static class PoolWorker extends Thread {
+        @Override
+        public void run() {
+
+            Runnable task;
+            while (true) {
+                synchronized (tasks) {
+                    while (tasks.isEmpty()) {
+                        try {
+                            tasks.wait();
+                        } catch (InterruptedException ex) {
+                            throw new IllegalStateException(ex);
+                        }
+                    }
+                    task = tasks.removeFirst();
+                }
+                try {
+                    task.run();
+                } catch (RuntimeException ex) {
+                    throw new IllegalStateException(ex);
+                }
+            }
+        }
+    }
+}
+
