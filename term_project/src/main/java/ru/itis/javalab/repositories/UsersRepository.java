a2716f22367f7d77c241434fa13acb80ecf4abfa
rosterfee <dakalom@mail.ru>
2022-12-31 12:00:00 +0300

    test

--- term_project/src/main/java/ru/itis/javalab/repositories/UsersRepository.java
+++ term_project/src/main/java/ru/itis/javalab/repositories/UsersRepository.java
@@ -1,9 +1,6 @@
 package ru.itis.javalab.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import ru.itis.javalab.models.User;
 
 import java.util.Optional;
